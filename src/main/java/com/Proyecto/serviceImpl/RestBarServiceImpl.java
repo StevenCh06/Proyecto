e2eabb4a25820a19c2247db364acff614e5874d7
alansagot <alansagotcespedes@gmail.com>
2023-10-28 11:45:12 -0600

    Creacion de package dao, domain,service y serviceImpl con sus respectivos datos

--- src/main/java/com/Proyecto/serviceImpl/RestBarServiceImpl.java
+++ src/main/java/com/Proyecto/serviceImpl/RestBarServiceImpl.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.Proyecto.serviceImpl;
 
-/**
- *
- * @author marlo
- */
-public class RestBarServiceImpl {
+import com.Proyecto.dao.RestBarDao;
+import com.Proyecto.domain.RestBar;
+import com.Proyecto.service.RestBarService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+
+public class RestBarServiceImpl implements RestBarService {
+     @Autowired
+    private RestBarDao restbarDao;
     
+    @Override
+    public RestBar getRestBar(RestBar restbar) {
+       return restbarDao.findById(restbar.getIdLocal()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(RestBar restbar) {
+        restbarDao.save(restbar);
+    }
+
+    @Override
+    @Transactional
+    public void delete(RestBar restbar) {
+        restbarDao.delete(restbar);
+    }
+
+
 }
